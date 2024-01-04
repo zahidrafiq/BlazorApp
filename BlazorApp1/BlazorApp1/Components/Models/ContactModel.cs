cdf06c3467e3d33c08d9889dc034c241a6f8395a
M. Zahid <hafizmzahid2@gmail.com>
2024-01-03 23:17:53 -0800

    Contact component created

---
+++ BlazorApp1/BlazorApp1/Components/Models/ContactModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Components.Models
+{
+    public class ContactModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
